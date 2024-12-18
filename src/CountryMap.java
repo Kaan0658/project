434a16be6302d256100e8e93f3f7304ed3f82d34
Kaan0658 <cokaanco@gmail.com>
2024-12-18 22:10:44 +0300

    spring boot first commit

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -1,19 +1,60 @@
 public class CountryMap {
-    public String A;
-    public String B;
-    public String C;
-    public String D;
-    public String E;
-    public int sehir = 5;
-    public int AB = 30;
-    public int AC = 10;
-    public int BD = 20;
-    public int CD = 50;
-    public int CE = 40;
-    public int DE = 10;
-    public int AE;
+    private String[] fileArray;
 
+    public CountryMap(String[] fileArray) {
+        this.fileArray = fileArray;
+    }
 
+    public boolean checkMapFormat() {
+
+        if (!checkFirstLine()){
+            return false;
+        }
+
+        String handler = fileArray[3];
+        String[] control = handler.split(" ");
+        String firstPath = control[0];
+        System.out.println(firstPath);
+        String secondPath = control[1];
+        System.out.println(secondPath);
+        if (firstPath.equals(secondPath)) {
+            System.out.println("Format is wrong they are same");
+            System.out.println("Line 4");
+            System.out.println(firstPath + " equals " + secondPath);
+            return false;
+        } else {
+            System.out.println("There is no problem");
+            return true;
+        }
+
+    }
+
+    private boolean checkFirstLine(){
+        boolean checkFirstLine = true;
+
+        String firstNumberString = fileArray[0];
+        boolean checkEmpty = false;
+        if (firstNumberString.equals("")){
+            checkEmpty = true;
+            firstNumberString = "First line is empty";
+        }
+        try{
+            int firstNumber = Integer.parseInt(firstNumberString);
+            System.out.println(firstNumber);
+        }
+        catch (NumberFormatException err){
+            if (!checkEmpty) {
+                System.out.println(firstNumberString + " is not a number");
+                checkFirstLine = false;
+            }
+            else {
+                System.out.println(firstNumberString);
+                checkFirstLine = false;
+            }
+        }
+
+        return checkFirstLine;
+    }
 }
 
 
